8c41e9eb3de0dcd53472ebcee1af1c1311398d6e
Sadik-Dev <oussama-sadik@outlook.com>
2020-10-15 14:06:22 +0200

    Initial Commit

---
+++ Assister/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Assister
+//
+//  Created by Sana on 15/10/2020.
+//  Copyright © 2020 Sadik-Dev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Assister/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Assister
+//
+//  Created by Sana on 15/10/2020.
+//  Copyright © 2020 Sadik-Dev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
